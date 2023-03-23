f98fed522b6d6e61619549c97e3bbee47203cee9
michaelmegally <michaelgamil8@gmail.com>
2023-03-23 14:06:53 +0200

    edit_commit

--- public/js/custom.js
+++ public/js/custom.js
@@ -89,23 +89,23 @@
 
 
 	// Menu elevator animation
-	// $('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
-	// 	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-	// 		var target = $(this.hash);
-	// 		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	// 		if (target.length) {
-	// 			var width = $(window).width();
-	// 			if(width < 991) {
-	// 				$('.menu-trigger').removeClass('active');
-	// 				$('.header-area .nav').slideUp(200);
-	// 			}
-	// 			$('html,body').animate({
-	// 				scrollTop: (target.offset().top) - 80
-	// 			}, 700);
-	// 			return false;
-	// 		}
-	// 	}
-	// });
+	$('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				var width = $(window).width();
+				if(width < 991) {
+					$('.menu-trigger').removeClass('active');
+					$('.header-area .nav').slideUp(200);
+				}
+				$('html,body').animate({
+					scrollTop: (target.offset().top) - 80
+				}, 700);
+				return false;
+			}
+		}
+	});
 
 	$(document).ready(function () {
 	    $(document).on("scroll", onScroll);
@@ -148,23 +148,6 @@
 	}
 
 
-	// Page loading animation
-	// $(window).on('load', function() {
-	// 	if($('.cover').length){
-	// 		$('.cover').parallax({
-	// 			imageSrc: $('.cover').data('image'),
-	// 			zIndex: '1'
-	// 		});
-	// 	}
-    //
-	// 	$("#preloader").animate({
-	// 		'opacity': '0'
-	// 	}, 600, function(){
-	// 		setTimeout(function(){
-	// 			$("#preloader").css("visibility", "hidden").fadeOut();
-	// 		}, 300);
-	// 	});
-	// });
 
 
 	// Window Resize Mobile Menu Fix
