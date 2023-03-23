027749619e64ce22301b827fa1e0d70d1cbf11ad
michaelmegally <michaelgamil8@gmail.com>
2023-03-23 11:34:15 +0200

    edit_commit

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,5 @@
 (function ($) {
-	
+
 	"use strict";
 	$('.owl-men-item').owlCarousel({
 		items:5,
@@ -69,7 +69,7 @@
 	    $("header").removeClass("background-header");
 	  }
 	});
-	
+
 
 	// Window Resize Mobile Menu Fix
 	mobileNav();
@@ -77,11 +77,11 @@
 
 	// Scroll animation init
 	window.sr = new scrollReveal();
-	
+
 
 	// Menu Dropdown Toggle
 	if($('.menu-trigger').length){
-		$(".menu-trigger").on('click', function() {	
+		$(".menu-trigger").on('click', function() {
 			$(this).toggleClass('active');
 			$('.header-area .nav').slideToggle(200);
 		});
@@ -89,37 +89,37 @@
 
 
 	// Menu elevator animation
-	$('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				var width = $(window).width();
-				if(width < 991) {
-					$('.menu-trigger').removeClass('active');
-					$('.header-area .nav').slideUp(200);	
-				}				
-				$('html,body').animate({
-					scrollTop: (target.offset().top) - 80
-				}, 700);
-				return false;
-			}
-		}
-	});
+	// $('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
+	// 	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	// 		var target = $(this.hash);
+	// 		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	// 		if (target.length) {
+	// 			var width = $(window).width();
+	// 			if(width < 991) {
+	// 				$('.menu-trigger').removeClass('active');
+	// 				$('.header-area .nav').slideUp(200);
+	// 			}
+	// 			$('html,body').animate({
+	// 				scrollTop: (target.offset().top) - 80
+	// 			}, 700);
+	// 			return false;
+	// 		}
+	// 	}
+	// });
 
 	$(document).ready(function () {
 	    $(document).on("scroll", onScroll);
-	    
+
 	    //smoothscroll
 	    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
 	        e.preventDefault();
 	        $(document).off("scroll");
-	        
+
 	        $('.scroll-to-section a').each(function () {
 	            $(this).removeClass('active');
 	        })
 	        $(this).addClass('active');
-	      
+
 	        var target = this.hash,
 	        menu = target;
 	       	var target = $(this.hash);
@@ -149,22 +149,22 @@
 
 
 	// Page loading animation
-	$(window).on('load', function() {
-		if($('.cover').length){
-			$('.cover').parallax({
-				imageSrc: $('.cover').data('image'),
-				zIndex: '1'
-			});
-		}
-
-		$("#preloader").animate({
-			'opacity': '0'
-		}, 600, function(){
-			setTimeout(function(){
-				$("#preloader").css("visibility", "hidden").fadeOut();
-			}, 300);
-		});
-	});
+	// $(window).on('load', function() {
+	// 	if($('.cover').length){
+	// 		$('.cover').parallax({
+	// 			imageSrc: $('.cover').data('image'),
+	// 			zIndex: '1'
+	// 		});
+	// 	}
+    //
+	// 	$("#preloader").animate({
+	// 		'opacity': '0'
+	// 	}, 600, function(){
+	// 		setTimeout(function(){
+	// 			$("#preloader").css("visibility", "hidden").fadeOut();
+	// 		}, 300);
+	// 	});
+	// });
 
 
 	// Window Resize Mobile Menu Fix
